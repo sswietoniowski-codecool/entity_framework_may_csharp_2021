31db3d6ffdb36f94e12994ba431a6b7b8fd97a99
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-01-08 19:16:16 +0100

    Added helper scripts for "Database First" approach

---
+++ create_table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Employees (
+    Id int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Salary varchar(255),
+    PassportInfoId varchar(255)
+);
---
+++ insert_data.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Employees (Id, FirstName, LastName, Salary)
+VALUES (2, 'Adam', 'KNowak', 0),
+       (3, 'Joanna', 'Bananowska', 0),
+       (5, 'Anna', 'Mimik', 0),
+	   (1, 'Jan', 'Kowalski', 5 )
