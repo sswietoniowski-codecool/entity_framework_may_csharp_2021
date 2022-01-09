0c37fc75416bd4a0c4f847098ed9c6a927884544
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-01-09 10:19:36 +0100

    Created CompanyContext and defined connection string

---
+++ Data/CompanyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class CompanyContext : DbContext
+    {
+        private const string ConnectionString = "Data Source=localhost;Database=Company_DB;Integrated Security=true";
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
