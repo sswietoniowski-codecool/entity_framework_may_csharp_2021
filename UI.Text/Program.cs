22c8646356ea54142f8c541766fa0735785ec3b5
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-01-09 11:42:09 +0100

    Demonstrated difference between First and Last

--- UI.Text/Program.cs
+++ UI.Text/Program.cs
@@ -14,22 +14,27 @@ namespace UI.Text
         {
             CompanyContext companyContext = new CompanyContext();
 
-            var employees = companyContext.Employees
-                //.Where(e => e.LastName.Contains("k")); // <- '%k%'
-                //.Where(e => EF.Functions.Like(e.LastName, "K%"));
-                //.ToList()
-                //.Where(e => e.LastNameStartsWithK());
-                //.Where(e => e.LastName.StartsWith("K"));
-                .Where(e => e.LastName != null);
+            //var employees = companyContext.Employees
+            //    //.Where(e => e.LastName.Contains("k")); // <- '%k%'
+            //    //.Where(e => EF.Functions.Like(e.LastName, "K%"));
+            //    //.ToList()
+            //    //.Where(e => e.LastNameStartsWithK());
+            //    //.Where(e => e.LastName.StartsWith("K"));
+            //    .Where(e => e.LastName != null);
 
-            foreach (var employee in employees)
-            {
-                Console.WriteLine(employee);
-            }
+            //foreach (var employee in employees)
+            //{
+            //    Console.WriteLine(employee);
+            //}
 
             //var employee = companyContext.Employees.FirstOrDefault(e => e.Id == 1);
             //var employee = companyContext.Employees.Find(1);
-            //Console.WriteLine(employee);
+            //var employee = companyContext.Employees.First();
+            var employee = companyContext.Employees
+                .Where(e => e.Id >= 2)
+                .OrderByDescending(e => e.Id)
+                .Last();
+            Console.WriteLine(employee);
         }
     }
 }
